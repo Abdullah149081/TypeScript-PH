bf171d7706661b796cd250bc12f56c3d1ca0cf1e
Abdullah149081 <masudmd425@gmail.com>
2023-11-02 10:48:30 +0600

    prettier formate

---
+++ module-1/src/object.ts
@@ -0,0 +1,7 @@
+let userData: { firstName: string; MiddleName?: string; lastName?: string } = {
+    firstName: 'Abdullah',
+    MiddleName: 'Al',
+    lastName: 'Masud',
+};
+
+console.log(userData);
